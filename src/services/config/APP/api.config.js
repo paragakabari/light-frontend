7aac8c9f609f363065f2314ebcbafbc97f2ab548
ParagNetwork <166380945+ParagNetwork@users.noreply.github.com>
2024-08-10 19:55:51 +0530

    add url

--- src/services/config/APP/api.config.js
+++ src/services/config/APP/api.config.js
@@ -1,5 +1,7 @@
 const protocol = "http";
-const host = "localhost:3002";
+// const host = "localhost:3002";
+const host = "13.202.5.33:3002";
+// http://13.202.5.33:3002/api/v1
 // const protocol = "http";
 // const host = "3.144.154.237";
 
